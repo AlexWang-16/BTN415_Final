796ad147eabe0dddd7db9edcefb021884aa9c3ae
Alex Wang <awang63@myseneca.ca>
2017-04-10 23:08:07 -0400

    * Reorganized files to better reflect each milestone
    * Initial commit for milestone 3 that compiles and at a blank state

---
+++ ms3/src/cpp/Main.cpp
@@ -0,0 +1,13 @@
+#ifndef MS3_MAIN
+#define MS3_MAIN
+#define _CRT_SECURE_NO_WARNINGS
+#include "../header/library.h"
+#include "../header/MySocket.h"
+
+using namespace std;
+int main() {
+
+
+  return 0;
+}
+#endif
---
+++ ms3/src/header/MySocket.h
@@ -0,0 +1,69 @@
+#ifndef MYSOCKET_H
+#define MYSOCKET_H
+#define NOMINMAX
+
+#include <windows.networking.sockets.h>
+#pragma comment(lib, "Ws2_32.lib")
+#include <iostream>
+#include <thread>
+#include <chrono>
+#include <limits>
+
+enum  SocketType { CLIENT, SERVER };
+enum  ConnectionType { TCP, UDP };
+const int DEFAULT_SIZE = 128;
+
+class MySocket {
+
+protected:
+	int version_num1, version_num2;
+	int port;
+	std::string IPAddr;
+	WSADATA wsa_data;
+	char *Buffer;
+
+	SOCKET WelcomeSocket, ConnectionSocket;
+
+	struct sockaddr_in SvrAddr;
+
+	SocketType mySocket;
+	ConnectionType connectionType;
+
+	bool bTCPConnect = false;		//True if connection is established
+
+	int MaxSize;
+
+public:
+	//constructor and destructor
+	MySocket(SocketType socketType, std::string, unsigned int, ConnectionType, unsigned int);
+	~MySocket();
+
+	//getter functions
+	int GetPort();
+	std::string GetIPAddr();
+	SocketType GetType();
+	ConnectionType GetConnectionType();
+	
+	//setter functions
+	void SetIPAddr(std::string ipAddress);
+	void SetPort(int portNumber);
+	void SetType(SocketType socketType);
+
+	//socket functions
+	void start_DLLS();
+	SOCKET initialize_tcp_socket();
+	SOCKET initialize_udp_socket();
+	void bind_socket();
+	void listen_socket();
+	void accept_connection();
+
+	//connection and transmission functions
+	void ConnectTCP();
+	void DisconnectTCP();
+	int GetData(char* recvBuffer);
+	void SendData(const char* data, int dataSize);
+
+};
+
+
+#endif MYSOCKET_H
